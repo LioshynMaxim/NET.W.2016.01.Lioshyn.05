8137ecda7049b4ae6faf5c53da5433f4fde133d1
Maksim Lioshyn <leshinmaxim@gmail.com>
2016-10-30 20:12:51 +0300

    Day5. Euclide&Stein Complete

--- Task1.Tests/EuclideanAlgorithmTests.cs
+++ Task1.Tests/EuclideanAlgorithmTests.cs
@@ -1,36 +1,51 @@
 ﻿using NUnit.Framework;
-using static Task1.EuclideanAlgorithm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static Task1.GreatestCommonDivisor;
 
 namespace Task1.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class EuclideanAlgorithmTests
     {
         [TestCase(24, 42, ExpectedResult = 6)]
         [TestCase(72, 64, ExpectedResult = 8)]
         [TestCase(64, 72, ExpectedResult = 8)]
         [TestCase(42, 24, ExpectedResult = 6)]
-        public int GreatestCommonSivisorTwoTest(int a, int b)
+        public int EuclideanGreatestCommonDivisorTwoTest(int a, int b)
         {
-            return GreatestCommonSivisor(a, b);
+            return EuclideanAlgorithm(a, b);
         }
 
-        [TestCase(new int[] { 36, 60, 42 }, ExpectedResult = 6)]
-        [TestCase(new int[] { 168, 180, 3024 }, ExpectedResult = 12)]
-        [TestCase(new int[] { 2340, 1080, 100 }, ExpectedResult = 20)]
-        [TestCase(new int[] { 2340, 1080, 100, 70 }, ExpectedResult = 10)]
-        [TestCase(new int[] { 2340, 1080, 100, 70, 140, 250 }, ExpectedResult = 10)]
-        public int GreatestCommonSivisorThreeTest(int[] arrInts)
+        [TestCase(new[] {36, 60, 42}, ExpectedResult = 6)]
+        [TestCase(new[] {168, 180, 3024}, ExpectedResult = 12)]
+        [TestCase(new[] {2340, 1080, 100}, ExpectedResult = 20)]
+        [TestCase(new[] {2340, 1080, 100, 70}, ExpectedResult = 10)]
+        [TestCase(new[] {2340, 1080, 100, 70, 140, 250}, ExpectedResult = 10)]
+        public int EuclideanGreatestCommonDivisorAnyTest(int[] arrInts)
         {
-            return GreatestCommonSivisor(arrInts);
+            return EuclideanAlgorithm(arrInts);
+        }
+    }
+
+    [TestFixture]
+    public class SteinAlgorithmTests
+    {
+        [TestCase(24, 42, ExpectedResult = 6)]
+        [TestCase(72, 64, ExpectedResult = 8)]
+        [TestCase(64, 72, ExpectedResult = 8)]
+        [TestCase(42, 24, ExpectedResult = 6)]
+        public int SteinGreatestCommonDivisorTwoTest(int a, int b)
+        {
+            return SteinAlgorithm(a, b);
         }
 
-
-
+        [TestCase(new[] {36, 60, 42}, ExpectedResult = 6)]
+        [TestCase(new[] {168, 180, 3024}, ExpectedResult = 12)]
+        [TestCase(new[] {2340, 1080, 100}, ExpectedResult = 20)]
+        [TestCase(new[] {2340, 1080, 100, 70}, ExpectedResult = 10)]
+        [TestCase(new[] {2340, 1080, 100, 70, 140, 250}, ExpectedResult = 10)]
+        public int SteinGreatestCommonDivisorAnyTest(int[] arrInts)
+        {
+            return SteinAlgorithm(arrInts);
+        }
     }
 }
---
+++ Task1/GreatestCommonDivisor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class GreatestCommonDivisor
+    {
+        public delegate int Algorithm(params int[] numbInts);
+
+        private static string timeAlgorithm = "";
+
+        #region Euclid
+
+        /// <summary>
+        /// The Euclidean algorithm calculates the greatest common divisor of two natural numbers a and b.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greatest common divisor of two natural numbers a and b</returns>
+        public static int EuclideanAlgorithm(int a, int b)
+        {
+            return b != 0?EuclideanAlgorithm(b, a % b):a;
+        }
+
+        /// <summary>
+        /// The Euclidean algorithm calculates the greatest common divisor of any natural numbers.
+        /// </summary>
+        /// <param name="numbInts">Array of natural numbers</param>
+        /// <returns>Greatest common divisor of any natural numbers</returns>
+        public static int EuclideanAlgorithm(params int[] numbInts)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int boof = EuclideanAlgorithm(numbInts[0], numbInts[1]);
+            for (int i = 2; i <= numbInts.Length - 1; i ++)
+            {
+                boof = EuclideanAlgorithm(boof, numbInts[i]);
+            }
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            timeAlgorithm = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+
+            return  boof;
+        }
+
+        #endregion
+
+
+        #region SteinAlgorithm
+
+        /// <summary>
+        /// The Stein algorithm calculates the greatest common divisor of any natural numbers.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greatest common divisor of two natural numbers a and b</returns>
+
+        public static int SteinAlgorithm(int a, int b)
+        {
+            if (a == 0) return b;
+            if (b == 0) return a;
+            if (a == b) return a;
+            if (a == 1 || b == 1) return 1;
+            if ((a % 2 == 0) && (b % 2 == 0)) return 2 * SteinAlgorithm(a / 2, b / 2);
+            if ((a % 2 == 0) && (b % 2 != 0)) return SteinAlgorithm(a / 2, b);
+            if ((a % 2 != 0) && (b % 2 == 0)) return SteinAlgorithm(a, b / 2);
+            return SteinAlgorithm(b, Math.Abs(a - b));
+        }
+
+        /// <summary>
+        /// The Stein algorithm calculates the greatest common divisor of any natural numbers.
+        /// </summary>
+        /// <param name="numbInts">Array of natural numbers</param>
+        /// <returns>Greatest common divisor of any natural numbers</returns>
+
+        public static int SteinAlgorithm(params int[] numbInts)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int boof = SteinAlgorithm(numbInts[0], numbInts[1]);
+            for (int i = 2; i <= numbInts.Length - 1; i++)
+            {
+                boof = SteinAlgorithm(boof, numbInts[i]);
+            }
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            timeAlgorithm = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds/10:00}";
+            return boof;
+        }
+        #endregion
+
+        #region TimeFunction
+
+        public static string ExecutionTimeOfAlgorithm(Algorithm algorithm)
+        {
+            return timeAlgorithm;
+        }
+        
+
+        #endregion
+    }
+
+    
+}
