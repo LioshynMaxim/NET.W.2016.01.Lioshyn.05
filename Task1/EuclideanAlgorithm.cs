d75a393936135ef2631032dd6f611b875404e5c9
Maksim Lioshyn <leshinmaxim@gmail.com>
2016-10-30 17:23:22 +0300

    Day5. Euclid

---
+++ Task1/EuclideanAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class EuclideanAlgorithm
+    {
+        public static int GreatestCommonSivisor()
+        {
+
+            return 1;
+        }
+    }
+}
